6aaa101e7d2d8569d974a9d6f1d44cd904209888
BranzkyCG <brandon.joao.c@gmail.com>
2024-05-01 04:00:36 -0400

    Fondo, matriz, hover y demases 1280 x 1024 OK

---
+++ comple.js
@@ -0,0 +1,101 @@
+//VERIFICACION DE VERDES (1280 X 1024)
+function verificar() {
+    let cuadrado = document.getElementById("TIERRA");
+    cuadrado.style.backgroundImage = "url('https://i.pinimg.com/736x/e9/70/f6/e970f64f231f8aef9eba8f63814a6f35.jpg')";
+    cuadrado.style.backgroundSize = "cover"; // Opcional: "contain" para ajustar la imagen manteniendo su relación de aspecto
+    alert("Encontraste al PLANETA TIERRA!");
+};
+
+function verificar1() {
+    let cuadrado = document.getElementById("MERCURIO");
+    cuadrado.style.backgroundImage = "url('https://i.pinimg.com/564x/2c/fc/bd/2cfcbd70b36129aced180d3c69b32d3c.jpg')";
+    cuadrado.style.backgroundSize = "cover"; // Opcional: "contain" para ajustar la imagen manteniendo su relación de aspecto
+    alert("Encontraste a MERCURIO!");
+};
+
+function verificar2() {
+    let cuadrado = document.getElementById("VENUS");
+    cuadrado.style.backgroundImage = "url('https://i.pinimg.com/564x/c2/92/66/c29266e0df1ac1121b42102e326777c9.jpg')";
+    cuadrado.style.backgroundSize = "cover"; // Opcional: "contain" para ajustar la imagen manteniendo su relación de aspecto
+    alert("Encontraste a VENUS!");
+};
+    
+function verificar3() 
+{
+    let cuadrado = document.getElementById("MARTE");
+    cuadrado.style.backgroundImage = "url('https://i.pinimg.com/564x/7b/39/ef/7b39ef2a94a38c06ae4da5a406a693a2.jpg')";
+    cuadrado.style.backgroundSize = "cover"; // Opcional: "contain" para ajustar la imagen manteniendo su relación de aspecto
+    alert("Encontraste a MARTE!");
+};
+    
+function verificar4() 
+{
+    let cuadrado = document.getElementById("JUPITER");
+    cuadrado.style.backgroundImage = "url('https://i.pinimg.com/564x/5f/c7/3d/5fc73dbc85e18d7164f51e6d408548cd.jpg')";
+    cuadrado.style.backgroundSize = "cover"; // Opcional: "contain" para ajustar la imagen manteniendo su relación de aspecto
+    alert("Encontraste a JUPITER!");
+};
+    
+function verificar5() 
+{
+    let cuadrado = document.getElementById("ESTRELLAS");
+    cuadrado.style.backgroundImage = "url('https://i.pinimg.com/564x/5f/c7/3d/5fc73dbc85e18d7164f51e6d408548cd.jpg')";
+    cuadrado.style.backgroundSize = "cover"; // Opcional: "contain" para ajustar la imagen manteniendo su relación de aspecto
+    alert("Haz chocado con una ESTRELLA!");
+};
+    
+function verificar6() 
+{
+    document.getElementById("cuadradoVerde6").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 7")
+};
+    
+function verificar7() 
+{
+    document.getElementById("cuadradoVerde7").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 8")
+};
+    
+function verificar8() 
+{
+    document.getElementById("cuadradoVerde8").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 9")
+};
+    
+function verificar9() 
+{
+    document.getElementById("cuadradoVerde9").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 10")
+};
+    
+function verificar10() 
+{
+    document.getElementById("cuadradoVerde10").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 11")
+};
+    
+function verificar11() 
+{
+    document.getElementById("cuadradoVerde11").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 12")
+};
+    
+function verificar12() 
+{
+    document.getElementById("cuadradoVerde12").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 13")
+};
+    
+function verificar13() 
+{
+    document.getElementById("cuadradoVerde13").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 14")
+};
+    
+function verificar14() 
+{
+    document.getElementById("cuadradoVerde14").style.backgroundColor = "green"
+    alert("ESTE ES EL CUADRADO NUMERO 15")
+};
+    
+
